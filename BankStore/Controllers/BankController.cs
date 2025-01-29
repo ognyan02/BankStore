6f74efadd34d7dbaf9dda78147a3905e642a00bd
ognyan02 <ognyan.georgiev2002@abv.bg>
2025-01-29 14:30:05 +0200

    mapper2

---
+++ BankStore/Controllers/BankController.cs
@@ -0,0 +1,65 @@
+﻿using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
+using BankStore.BL.Interfaces;
+using BankStore.Models.DTO;
+using BankStore.Models.Requests;
+using System.Runtime.CompilerServices;
+namespace BankStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BankController : ControllerBase
+    {
+        private readonly IBankservises _bankservises;
+        private readonly IMapper _mapper;
+
+        public BankController(IBankservises bankservises, IMapper mapper)
+        {
+            _bankservises = bankservises;
+            _mapper = mapper;
+        }
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _bankservises.GetAll();
+
+            if (result != null && result.Count > 0)
+            {
+                return Ok(result);
+            }
+            return NotFound();
+
+        }
+
+        [ProducesResponseType(StatusCode.Status200Ok)]
+        [ProducesResponseType(StatusCode.Status4004NotFound)]
+        [ProducesResponseType(StatusCode.Status400BadRequest)]
+        [HttpGet("GetById")]
+        public IActionResult GetById(string id)
+        {
+            if(string.IsNullOrEmpty(id))
+            {
+                return BadRequest($"Wrong ID: {id}");
+            }
+            var result = _bankservises.GetById(id);
+            if (result == null)
+            {
+                return NotFound($"Team with ID:{id} not found");
+            }
+            return Ok(result);
+        }
+        [HttpPost("Add")]
+        public IActionResult Add([FromBody]AddBankRequests bank)
+        {
+            var BankDTO = _mapper.Map<Bank>(bank);
+            _bankservises.Add(BankDTO);
+                return Ok();
+        }
+        [HttpDelete("Delete")]
+        public void Delete (int id)
+        {
+           // return _bankservises.GetById(id);
+        }
+
+    }
+}
---
+++ BankStore/Controllers/BussinesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BankStore.BL.Interfaces;
+using BankStore.Models.DTO;
+using BankStore.Models.Requests;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+namespace BankStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BussinesController : ControllerBase
+    {
+        private readonly IBussinesservises _bankservisses;
+        public BussinesController(IBussinesservises bankservisses)
+        {
+            _bankservisses = bankservisses;
+        }
+        [HttpGet("GetAllDetailedBank")]
+        public IActionResult GetAllDetailedBank()
+        {
+            var result = _bankservisses.GetAllBank();
+            if(result!=null && result.Count >0)
+            {
+                return Ok(result);
+            }
+            return NotFound();
+        }
+        [HttpPost("Test")]
+        public IActionResult Test([FromBody]  TestRequest bank)
+        {
+            return Ok();
+        }
+    }
+    public class TestRequest
+        {
+        public int MagicNumber { get; set; }
+        public string Description {  get; set; }
+        public DateTime DateTime { get; set; }
+        }
+}
