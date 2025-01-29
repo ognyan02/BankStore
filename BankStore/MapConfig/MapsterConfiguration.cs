628abf5af693d44800b2eeb7ffc415066177f395
ognyan02 <ognyan.georgiev2002@abv.bg>
2025-01-29 13:51:01 +0200

    mapster

---
+++ BankStore/MapConfig/MapsterConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Mapster;
+using BankStore.Models.DTO;
+using BankStore.Models.Requests;
+namespace BankStore.MapConfig
+{
+    public class MapsterConfiguration
+    {
+        public static void Configure()
+        {
+            TypeAdapterConfig<AddBankRequests, Bank>
+                .NewConfig();
+        }
+    }
+}
